333aac015a0b5c413ecd01329f3038dbf40ccef7
Dariusz Golomski <dariusz.golomski@example.com>
2020-10-23 11:31:27 +0200

    pr1 end 2

--- src/main/java/pracownia/introduction/HelloWorld.java
+++ src/main/java/pracownia/introduction/HelloWorld.java
@@ -1,8 +1,12 @@
 package pracownia.introduction;
 
+import org.apache.log4j.Logger;
+
 public class HelloWorld {
 
     public static void main(String [ ] args) {
+        Logger logger = Logger.getLogger(HelloWorld.class);
+        logger.info("message");
         System.out.println("Hello World");
     }
 }
